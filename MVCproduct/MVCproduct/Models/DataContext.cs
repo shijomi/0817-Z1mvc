82e17ec37df71ab01c41d092ff420882e7cc7a80
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 10:11:13 -0400

    Solucion completa MVC

---
+++ MVCproduct/MVCproduct/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCproduct.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCproduct.Models.Product> Products { get; set; }
+    }
+}
---
+++ MVCproduct/MVCproduct/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCproduct.Models
+{
+
+    public class Product
+    {   [Key]
+        public int ProductID { get; set; }
+        [Required]
+        [Display(Description ="Product Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
